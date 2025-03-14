d0257db13436badcb6fa280b26e4f379b0f7a61d
Halfdan <halfdansc@gmail.com>
2025-03-14 08:15:31 +0100

    finished the controller before database will be changes when a data base is added

--- Controllers/ScoresController.cs
+++ Controllers/ScoresController.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using poke_poke.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace poke_poke.Controllers
-
 {
     [Route("api/[controller]")]
     [ApiController]
@@ -11,11 +13,57 @@ namespace poke_poke.Controllers
     {
         private static List<GameScore> scores = new List<GameScore>();
 
-        // GET method
+        // GET all scores
         [HttpGet]
-        public ActionResult<IEnumerable<GameScore>> GetScores() 
+        public ActionResult<IEnumerable<GameScore>> GetScores()
         {
             return Ok(scores);
+        }
+
+        // GET a single score by ID
+        [HttpGet("{id}")]
+        public ActionResult<GameScore> GetScore(long id)
+        {
+            var gameScore = scores.Find(x => x.Id == id);
+
+            if (gameScore == null)
+            {
+                return NotFound($"No score found with ID {id}");
+            }
+
+            return Ok(gameScore);
+        }
+
+        // POST a new score
+        [HttpPost]
+        public ActionResult<GameScore> PostScore(GameScoreDTO scoreDTO)
+        {
+            var gameScore = new GameScore
+            {
+                Id = scores.Any() ? scores.Max(s => s.Id) + 1 : 1,
+                PlayerName = scoreDTO.PlayerName,
+                Score = scoreDTO.Score,
+                TimeOfScore = scoreDTO.TimeOfScore,
+            };
+
+            scores.Add(gameScore);
+
+            return CreatedAtAction(nameof(GetScore), new { id = gameScore.Id }, gameScore);
+        }
+
+        // DELETE a score by ID
+        [HttpDelete("{id}")]
+        public ActionResult DeleteScore(long id)
+        {
+            var score = scores.Find(x => x.Id == id);
+            if (score == null)
+            {
+                return NotFound();
+            }
+
+            scores.Remove(score);
+
+            return NoContent();
         }
     }
 }
