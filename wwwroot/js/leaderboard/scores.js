b52f827f56688c27e3f45f24a0ca56a78d814b44
Halfdan <halfdansc@gmail.com>
2025-03-17 14:14:08 +0100

    now the leaderboard is sorted and shows the date in a nicer format

--- wwwroot/js/leaderboard/scores.js
+++ wwwroot/js/leaderboard/scores.js
@@ -3,19 +3,46 @@ import { getScores } from "../objectAndFetch/fetchTokenAndScore.js";
 // scorelist ol from leaderboard.html
 const scoreList = document.getElementById("score-list");
 
+const sortScores = (scores) => {
+    return scores.sort((a, b) => b.score - a.score); // Sort by highest score
+};
 
-// TODO: find a way to sort the scores by highest score
+const formattedDate = (date) => {
+    return new Date(date).toLocaleString("en-DK", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        // second: "2-digit", uncomment to dispaly seconds aswell
+        hour12: false,
+    });
+};
+
 const displayScores = async () => {
-
     const scores = await getScores();
-    console.log(scores);
-    for (let score of scores) {
+
+    if (!Array.isArray(scores)) {
+        console.error("Error: expected array");
+        return;
+    }
+    const sortedScores = sortScores(scores);
+    let counter = 0;
+
+    for (let score of sortedScores) {
+        if (counter > 25) {
+            break;
+        }
+
         let element = document.createElement('li');
         let span = document.createElement('span');
+        let date = formattedDate(score.timeOfScore);
 
-        span.innerHTML = `Player: ${score.playerName} ->  Score: ${score.score} -> Date: ${score.timeOfScore}`;
+        span.innerHTML = `Player: ${score.playerName} ->  Score: ${score.score} -> Date: ${date}`;
         element.appendChild(span);
         scoreList.appendChild(element);
+        
+        counter++;
     }
 
 };
