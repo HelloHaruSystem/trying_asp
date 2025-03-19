71a3bb81c8459678da8d1b7fc0456be5cf61ed16
Halfdan <halfdansc@gmail.com>
2025-03-19 09:24:12 +0100

    Made the user able to search for playernames when looking at scores

--- wwwroot/js/leaderboard.js
+++ wwwroot/js/leaderboard.js
@@ -1,6 +1,23 @@
 import { setLogoAndPochama } from "./style/pochama.js";
-import { displayScores } from "./leaderboard/scores.js";
+import { displayScores, searchScores } from "./leaderboard/scores.js";
+
+// search for your score form
+const searchForm = document.getElementById("search-score");
 
 // run at startup Methods
 setLogoAndPochama();
 displayScores();
+
+// event handlers
+searchForm.addEventListener('submit', (event) => {
+    event.preventDefault(); // prevents form from reloading the page
+
+    // input value (name) from html form
+    const playerName = document.getElementById('search-name').value;
+
+    if (playerName === "") {
+        displayScores();
+    } else {
+        searchScores(playerName);
+    }
+});
--- wwwroot/js/leaderboard/scores.js
+++ wwwroot/js/leaderboard/scores.js
@@ -3,7 +3,17 @@ import { getScores } from "../objectAndFetch/fetchTokenAndScore.js";
 // scorelist ol from leaderboard.html
 const scoreList = document.getElementById("score-list");
 
-const sortScores = (scores) => {
+// the number that list will start from
+let playerNumber = 0;
+
+const getAndSortScores = async () => {
+    const scores = await getScores();
+
+    if (!Array.isArray(scores)) {
+        console.error("Error: expected array");
+        return;
+    }
+
     return scores.sort((a, b) => b.score - a.score); // Sort by highest score
 };
 
@@ -19,32 +29,13 @@ const formattedDate = (date) => {
     });
 };
 
-const displayScores = async () => {
-    const scores = await getScores();
+const addScores = (playerName, score, date) => {
+    let element = document.createElement('li');
+    let span = document.createElement('span');
 
-    if (!Array.isArray(scores)) {
-        console.error("Error: expected array");
-        return;
-    }
-    const sortedScores = sortScores(scores);
-    let counter = 0;
-
-    for (let score of sortedScores) {
-        if (counter > 25) {
-            break;
-        }
-
-        let element = document.createElement('li');
-        let span = document.createElement('span');
-        let date = formattedDate(score.timeOfScore);
-
-        span.innerHTML = `Player: ${score.playerName} ->  Score: ${score.score} -> Date: ${date}`;
-        element.appendChild(span);
-        scoreList.appendChild(element);
-        
-        counter++;
-    }
-
+    span.innerHTML = `Player: ${playerName} ->  Score: ${score} -> Time and date: ${date}`;
+    element.appendChild(span);
+    scoreList.appendChild(element);
 };
 
 const clearScores = () => {
@@ -53,4 +44,55 @@ const clearScores = () => {
     }
 };
 
-export { displayScores };
+const scoreNotFound = () => {
+    let element = document.createElement('li');
+    let span = document.createElement('span');
+
+    span.innerHTML = `No players found`;
+    element.appendChild(span);
+    scoreList.appendChild(element);
+};
+
+const displayScores = async () => {
+    const sortedScores = await getAndSortScores();
+    clearScores();
+    let counter = 0;
+
+    for (let score of sortedScores) {
+        if (counter > 25) {
+            break;
+        }
+
+        let date = formattedDate(score.timeOfScore);
+
+        addScores(score.playerName, score.score, date);
+        counter++;
+    }
+    if (counter === 0) {
+        scoreNotFound();
+    }
+};
+
+const searchScores = async (playerName) => {
+    const sortedScores = await getAndSortScores();
+    clearScores();
+    let counter = 0;
+
+    for (let score of sortedScores) {
+        if (counter > 25) {
+            break;
+        }
+        
+        if (playerName.toLowerCase() === score.playerName.toLowerCase()) {
+            let date = formattedDate(score.timeOfScore);
+
+        addScores(score.playerName, score.score, date);
+        counter++;
+        }
+    }
+    if (counter === 0) {
+        scoreNotFound();
+    }
+};
+
+export { displayScores, searchScores };
