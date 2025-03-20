8856779060005dac0d1b96702038c8135e369f6e
Halfdan <halfdansc@gmail.com>
2025-03-20 14:16:43 +0100

    fixed a namespace issue

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 
-[Route("/")]
-public class HomeController : Controller
+namespace poke_poke.Controllers
 {
-    // IWebHostEnviroment is the path to the wwwroot folder
-    private readonly IWebHostEnvironment _env;
-
-    public HomeController(IWebHostEnvironment env)
+    [Route("/")]
+    public class HomeController : Controller
     {
-        _env = env;
-    }
+        // IWebHostEnviroment is the path to the wwwroot folder
+        private readonly IWebHostEnvironment _env;
 
-    [HttpGet]
-    public IActionResult Index()
-    {
-        var filePath = Path.Combine(_env.WebRootPath, "index.html");
-        return PhysicalFile(filePath, "text/html");
+        public HomeController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var filePath = Path.Combine(_env.WebRootPath, "index.html");
+            return PhysicalFile(filePath, "text/html");
+        }
     }
 }
